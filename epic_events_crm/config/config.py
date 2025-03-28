61dc2eaf7638bc6ca6c7d3c861e17e1569ac8586
Damien Vincelot <damien.vincelot@gmail.com>
2025-03-28 10:39:25 +0100

    fix dotenv bug in .env

--- epic_events_crm/config/config.py
+++ epic_events_crm/config/config.py
@@ -1,4 +1,5 @@
 import os
+import sentry_sdk
 
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
@@ -6,6 +7,13 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 # Charger les variables d'environnement depuis le fichier .env
 load_dotenv()
+
+sentry_sdk.init(
+    dsn="https://d25274d49594994e87a357fda009f962@o4509044061437952.ingest.de.sentry.io/4509044064649296",  # noqa: E501
+    # Add data like request headers and IP for users,
+    # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info  # noqa: E501
+    send_default_pii=True,
+)
 
 # Récupérer la clé secrète pour JWT depuis le fichier .env
 SECRET_KEY = os.getenv("SECRET_KEY")
